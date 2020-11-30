6571054e10ecda1e3db231b2588d2ba3892c9849
Biye Li <libiye@gmail.com>
2020-11-30 01:00:23 -0500

    multi head attn

--- modules/attention.py
+++ modules/attention.py
@@ -16,3 +16,30 @@ class ScaledDotProductAttention(nn.Module):
         return res
 
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, h, d_model, d_K, d_V, use_mask=False):
+        super(MultiHeadAttention, self).__init__()
+        self.h = h
+        self.sdp_attn = ScaledDotProductAttention(use_mask)
+        self.W_Q = torch.rand(d_model, d_K * h)
+        self.W_K = torch.rand(d_model, d_K * h)
+        self.W_V = torch.rand(d_model, d_V * h)
+        self.W_O = torch.rand(d_V * h, d_model)
+        
+
+    def forward(self, Q, K, V):
+        h = self.h
+        d_V = V.shape[1]
+        d_K = K.shape[1]
+
+        # shape d_K by (d_V * h)
+        head = torch.zeros(d_K, d_V * h)
+        
+        for i in range(h):
+            nq = Q.bmm(W_Q[:, i * d_K])
+            nk = K.bmm(W_K[:, i * d_K])
+            nv = V.bmm(W_V[:, i * d_V])
+            head[:, i * d_V] = self.spd_attn(nq, nk, nv)
+
+        res = head.bmm(W_O)
+        return res
